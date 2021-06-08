00395e2c0096ade6ea7682d472a2f48f9df9f04f
Felipe Marques Ramos <felipemr94@outlook.com>
2021-06-08 14:22:11 -0300

    Created button actions so the game now really works

--- GuessGame/ViewController.swift
+++ GuessGame/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
 
     override func viewDidLoad() {
@@ -31,17 +32,39 @@ class ViewController: UIViewController {
     }
 
     
-    func askQuestion(){
+    func askQuestion(action : UIAlertAction! = nil){
         countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
 
+        title = countries[correctAnswer].uppercased()
     }
     
     func stylizeButton(button: UIButton){
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title : String
+        
+        if sender.tag == correctAnswer{
+            title = "correct"
+            score += 1
+        }
+        else{
+            title = "wrong"
+            score -= 1
+        }
+        
+        let alertController = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(alertController, animated: true)
     }
 }
 
