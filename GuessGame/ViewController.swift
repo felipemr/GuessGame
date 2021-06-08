aeb0a3a22b0ffe44badb57961d6e0dd3447b259b
Felipe Marques Ramos <felipemr94@outlook.com>
2021-06-08 13:40:23 -0300

    Buttons now have the expected appearance so the game is actually playable

--- GuessGame/ViewController.swift
+++ GuessGame/ViewController.swift
@@ -13,11 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        stylizeButton(button: button1)
+        stylizeButton(button: button2)
+        stylizeButton(button: button3)
+        
+        askQuestion()
+
     }
 
+    
+    func askQuestion(){
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+
+    }
+    
+    func stylizeButton(button: UIButton){
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+    }
 }
 
